ad6b9cce260fa4c81ee823ee96ff503aa37b230a
Wilmer <wilmersinchi@gmail.com>
2021-04-16 18:41:49 -0400

    functionality for date

---
+++ src/components/MainPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import DatePicker from '../components/Datepicker'
+
+const MainPage = () => {
+   return (
+      <div>
+         <DatePicker />
+      </div>
+   )
+}
+export default MainPage
